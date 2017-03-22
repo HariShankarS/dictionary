da4fef728a518ad9373f52094c906e0db4740ba4
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2017-03-22 16:06:27 +0530

    Extended search

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -5,7 +5,8 @@ class WordsController < ApplicationController
   # GET /words.json
   def index
     if params[:search].present?
-      @words = Word.joins(:meanings).where("words.term ILIKE ? or meanings.definition ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").uniq.paginate(page: params[:page], per_page: 4)
+      @words = Word.joins(:meanings).where("words.term ILIKE ?", "%#{params[:search]}%")
+      @words_definitions = Word.joins(:meanings).where("meanings.definition ILIKE ?","%#{params[:search]}%").uniq.paginate(page: params[:page], per_page: 4)
     else
       @words = Word.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
     end
