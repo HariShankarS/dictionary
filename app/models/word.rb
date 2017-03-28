76b8046ad7c401a752684b3a3eb280e47d198429
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2017-03-28 20:55:03 +0530

    Added back validation after commenting CSV

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,10 @@
 class Word < ApplicationRecord
-  has_many :meanings, inverse_of: :word
-  has_many :examples, inverse_of: :word
-  accepts_nested_attributes_for :meanings, reject_if: :all_blank, allow_destroy: true
-  accepts_nested_attributes_for :examples, reject_if: :all_blank, allow_destroy: true
-  require 'csv'
+   has_many :meanings, inverse_of: :word
+   has_many :examples, inverse_of: :word
+   validates_uniqueness_of :term
+   validates_presence_of :term
+   validates_presence_of :meanings
+   accepts_nested_attributes_for :meanings, reject_if: :all_blank, allow_destroy: true
+   accepts_nested_attributes_for :examples, reject_if: :all_blank, allow_destroy: true
+   require 'csv'
 end
